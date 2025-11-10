8235048f55b3cbc86f283ee91e49372670e41950
Whishes <nicholas.cruickshank25@gmail.com>
2025-11-10 16:45:09 +1100

    added Player scene to figure out movement, limited movement to 4 directions, added Level_1 scene to properly test movement (will be improved very soon)

---
+++ scripts/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+var speed = 100
+var direction = Vector2.ZERO
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+#func get_input():
+	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+func _physics_process(delta):
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	# Stops movement if not pressing any input above
+	if input_dir == Vector2.ZERO: direction = Vector2.ZERO
+	
+	if (Input.is_action_pressed("ui_left")):
+		direction = Vector2(input_dir.x, 0).normalized()
+		animated_sprite.play("idle_left")
+		
+	if (Input.is_action_pressed("ui_right")):
+		direction = Vector2(input_dir.x, 0).normalized()
+		animated_sprite.play("idle_right")
+	
+	if (Input.is_action_pressed("ui_down")):
+		direction = Vector2(0, input_dir.y).normalized()
+		animated_sprite.play("idle_down")
+	
+	if (Input.is_action_pressed("ui_up")):
+		direction = Vector2(0, input_dir.y).normalized()
+		animated_sprite.play("idle_up")
+	
+	velocity = direction * speed
+	move_and_slide()
