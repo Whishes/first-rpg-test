0d3858d79d23cf888bb313fc1f1ab435918a0fe8
Whishes <nicholas.cruickshank25@gmail.com>
2025-11-11 17:27:11 +1100

    fixed changing scenes during a physics tick by deffering the call until during idle time

--- scripts/move_to_area.gd
+++ scripts/move_to_area.gd
@@ -6,6 +6,8 @@ func _on_body_entered(body: Node2D) -> void:
 		return
 	var collision_child: CollisionShape2D = get_child(0)
 	var level_to_move = collision_child.name
-	
+	var file_path_new_level = "res://scenes/levels/%s.tscn" % level_to_move
 	
 	print(level_to_move)
+	get_tree().call_deferred("change_scene_to_file", file_path_new_level)
+	#call_deferred("queue_free")
